bc9c415e8e0670fc294d08828201f80c045da040
eric <howcrazys@gmail.com>
2024-10-31 17:27:10 +0800

    feat: 抽出 selectbase

---
+++ src/components/base/SelectBase.vue
@@ -0,0 +1,96 @@
+<!-- CustomSelect.vue -->
+<script setup>
+import { CaretBottom } from "@element-plus/icons-vue";
+
+// 定義 props
+defineProps({
+  // modelValue: {
+  //   type: [String, Number],
+  //   default: "",
+  // },
+  placeholder: {
+    type: String,
+    default: "請選擇",
+  },
+  customClass: {
+    type: String,
+    default: "customSelect",
+  },
+  style: {
+    type: Object,
+    default: () => ({
+      width: "240px",
+    }),
+  },
+});
+
+const modelValue = defineModel();
+// 定義 emits
+// const emit = defineEmits(["update:modelValue"]);
+
+// 處理值的變化
+// const handleChange = (val) => {
+//   emit("update:modelValue", val);
+// };
+</script>
+
+<template>
+  <!-- :model-value="modelValue"
+      @update:modelValue="handleChange" -->
+  <el-select
+    :suffix-icon="CaretBottom"
+    v-model="modelValue"
+    :placeholder="placeholder"
+    :style="{ ...style }"
+    :popper-class="customClass"
+  >
+    <slot></slot>
+    <template #prefix>
+      <slot name="prefix"></slot>
+    </template>
+    <template #footer>
+      <slot name="footer"></slot>
+    </template>
+  </el-select>
+</template>
+
+<style lang="scss">
+// 調整下拉箭頭樣式
+.el-icon {
+  &.el-select__caret {
+    color: #000;
+    font-size: 20px;
+  }
+}
+
+// customSelect 是自定義的 class
+.customSelect {
+  &.el-select__popper {
+    // display: block !important;
+    border-radius: 20px;
+  }
+  .el-select {
+    &-dropdown {
+      padding: {
+        top: 20px;
+        bottom: 20px;
+      }
+    }
+    &-dropdown__wrap {
+      max-height: 100px;
+      overflow: scroll;
+    }
+    &-dropdown__item {
+      &.is-selected {
+        color: #000;
+        background-color: pink;
+      }
+    }
+  }
+
+  // 調整箭頭樣式
+  .el-popper__arrow.el-popper__arrow.el-popper__arrow::before {
+    background-color: blue;
+  }
+}
+</style>
---
+++ src/components/base/selectEnter.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-input placeholder="請輸入"> </el-input>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+$input-color: lightgray;
+$input-border-color: darken($input-color, 50%);
+
+.el-input {
+  &__wrapper {
+    background-color: $input-color;
+    border: 1px solid $input-border-color;
+    border-radius: 20px;
+    &.is-focus {
+      // 處理的看起來都是 box-shadow
+      box-shadow: 0 0 10px $input-border-color;
+    }
+  }
+
+  &__inner {
+    height: auto;
+    padding: {
+      top: 10px;
+      bottom: 10px;
+    }
+  }
+}
+</style>
